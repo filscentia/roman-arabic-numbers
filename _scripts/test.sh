84541f0f23ed100831a9cb117bea8aa338e9da16
mbwhite <matthew@mh-white.com>
2017-08-12 20:49:04 +0000

    final go at the test build

--- _scripts/test.sh
+++ _scripts/test.sh
@@ -8,10 +8,10 @@ TEST="${DIR}/../_test"
 GENERAL="${DIR}/../_general"
 
 cd ${GENERAL}
-for LNG in *;
+for LNG in nodejs java;
 do
   cd "${GENERAL}/${LNG}"
-  
+  pwd
   npm test -- --file "${TEST}/input.csv" | awk '/--START--/{flag=1;next}/--END--/{flag=0;}flag' > temp.out
   diff --side-by-side  --suppress-common-lines temp.out "${TEST}/results.csv"
 
