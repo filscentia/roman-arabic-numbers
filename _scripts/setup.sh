462aa238391a7dbf6dc68fd8d3ad42975d4d4df0
mbwhite <matthew@mh-white.com>
2017-08-12 20:21:38 +0000

    test 2

--- _scripts/setup.sh
+++ _scripts/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -ev
+set -o pipefail
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}")" && pwd )"
 GENERAL="${DIR}/../_general"
  
--- _scripts/test.sh
+++ _scripts/test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -ev
+set -o pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}")" && pwd )"
 
